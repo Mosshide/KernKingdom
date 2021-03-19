7855bbf55bf3c1da284cbdee9002a349a6b164a0
QUINN\Wyatt <wyattsushi@gmail.com>
2021-03-19 09:53:29 -0700

    updated sections to take up proper space

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+class Building {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  
+  distance(a, b) {
+    return Math.hypot(a, b);
+  }
+}
+
 window.addEventListener('wheel', function(e) {
-  if (e.deltaY !== 0) document.documentElement.scrollLeft += e.deltaY;
+  if (e.deltaY !== 0) {
+	  document.documentElement.scrollTop -= e.deltaY;
+	  document.documentElement.scrollLeft += e.deltaY;
+  }
 });
